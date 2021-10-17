ca6776cb1d90999ca2ba10958aeaf96b045da38e
permanaheri <heripermana88@gmail.com>
2021-10-17 23:07:48 +0700

    nsq mentoring test

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const passport = require("passport");
+const nsq = require('nsqjs')
 
-var authRouter = require('./routes/auth');
+const authRouter = require('./routes/auth');
 
 const app = express();
 const port = 3000
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+const reader = new nsq.Reader('test_login', 'test_login', {
+    lookupdHTTPAddresses: 'localhost:4161'
+})
 
 app.use('/', authRouter);
+
+reader.connect()
+
+reader.on('message', msg => {
+    console.log('Received message [%s]: %s', msg.id, msg.body.toString())
+    msg.finish()
+  })
 
 app.listen(port, () => {
     console.log(`Server started on port ${port} http://localhost:${port}`);
--- routes/auth.js
+++ routes/auth.js
@@ -1,17 +1,43 @@
-var express = require('express');
-var passport = require('passport');
+const express = require('express');
+const passport = require('passport');
+const nsq = require('nsqjs');
 
-var router = express.Router();
+const router = express.Router();
+const nsqWriter = new nsq.Writer('127.0.0.1', 4150);
+
+const isWriter = function (req) {
+
+  nsqWriter.connect()
+
+  nsqWriter.on('ready', () => {
+    const message = { host: req.headers.host , auth_basic: req.headers.authorization}
+    nsqWriter.publish('test_login', message, err => {
+      if (err) { return console.error(err.message) }
+      console.log('basic auth sent message')
+      nsqWriter.close()
+    })
+  })
+
+
+  nsqWriter.on('closed', () => {
+    console.log('Writer closed')
+  })
+
+  return true;
+}
 
 const midPassport = require('../lib/passport');
 
 router.use(passport.initialize());
 
-router.get('/auth/detail/:id', midPassport.isAuthenticated, function (req, res, next) {
-  res.send({
-    status: 'OK',
-    data:req.params
-  })
+router.get('/auth/detail/:id', midPassport.isAuthenticated, async function (req, res, next) {
+  const doWrite = await isWriter(req);
+  if (doWrite) {
+    res.send({
+      status: 'OK',
+      data: req.params
+    })
+  }
 });
 
 module.exports = router;
